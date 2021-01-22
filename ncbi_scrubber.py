a6188a72fd00b5b3b47c38fb6b252b3d9cf24efd
Casey Dunn <casey.dunn@yale.edu>
2021-01-21 21:36:17 -0500

    working

--- ncbi_scrubber.py
+++ ncbi_scrubber.py
@@ -1,10 +1,25 @@
-import argparse
+#!/usr/bin/env python
+import sys
 from Bio import SeqIO
-
-sequences = []
-
-def main()
+import re
 
 if __name__ == "__main__":
-    # execute only if run as a script
-    main()
+    records = []
+    ids = set()
+    for file_name in sys.argv[1:]:
+        for record in SeqIO.parse(file_name, "fasta"):
+            id = record.id
+            taxon = "taxon"
+            bracket_match = re.search(r"\[(.+?)\]", record.description)
+            if bracket_match!=None:
+                taxon=bracket_match.group(1)
+            record.name = taxon
+            
+            # Only add the record if it is new
+            if not id in ids:
+                ids.add(id)
+                records.append(record)
+    
+    for record in records:
+        print(">" + record.name.replace(" ", "_") + "@" + record.id )
+        print(str(record.seq))
